3699a5e1a144a6630f8532a39e4ab6d238bcd4b5
jugof <jugisoff@gmail.com>
2024-04-18 18:35:54 +0530

    18-04-2024evening

--- CodingSamples/Java/Platform-Enterprise/JDBCTest/demo2/src/main/java/app/Program.java
+++ CodingSamples/Java/Platform-Enterprise/JDBCTest/demo2/src/main/java/app/Program.java
@@ -2,6 +2,7 @@ package app;
 
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Program {
     
@@ -11,6 +12,29 @@ public class Program {
         int quantity = Integer.parseInt(args[2]);
         Date today = new Date(System.currentTimeMillis());
         var con = DriverManager.getConnection("jdbc:oracle:thin:@//iitdac.met.edu/xe", "scott", "tiger");
+        con.setAutoCommit(false);
+        try{
+            var stmt = con.createStatement();
+            stmt.executeUpdate("update counters set cur_val=cur_val+1 where ctr_name='orders'");
+            var rs = stmt.executeQuery("select cur_val+1000 from counters where ctr_name='orders'");
+            rs.next();
+            int orderNo = rs.getInt(1);
+            rs.close();
+            stmt.close();
+            var pstmt = con.prepareStatement("insert into orders values(?, ?, ?, ?, ?)");
+            pstmt.setInt(1, orderNo);
+            pstmt.setDate(2, today);
+            pstmt.setString(3, customerId);
+            pstmt.setInt(4, productNo);
+            pstmt.setInt(5, quantity);
+            pstmt.executeUpdate();
+            pstmt.close();
+            con.commit();
+            System.out.printf("New Order Number: %d%n", orderNo);
+        }catch(SQLException e){
+            con.rollback();
+            System.out.printf("Order Failed: %s%n", e.getMessage());
+        }
         con.close();
     }
 }
---
+++ CodingSamples/Java/Platform-Enterprise/JDBCTest/demo3/src/main/java/app/Program.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class Program {
+    
+    public static void main(String[] args) throws Exception {
+        String customerId = args[0].toUpperCase();
+        int productNo = Integer.parseInt(args[1]);
+        int quantity = Integer.parseInt(args[2]);
+        var con = DriverManager.getConnection("jdbc:oracle:thin:@//iitdac.met.edu/xe", "scott", "tiger");
+        var cstmt = con.prepareCall("{call place_order(?, ?, ?, ?)}");
+        cstmt.setString(1, customerId);
+        cstmt.setInt(2, productNo);
+        cstmt.setInt(3, quantity);
+        cstmt.registerOutParameter(4, Types.INTEGER);
+        try{
+            cstmt.execute();
+            System.out.printf("New Order Number: %d%n", cstmt.getInt(4));
+        }catch(SQLException e){
+            System.out.printf("Order Failed: %s%n", e.getMessage());
+        }
+        cstmt.close();
+        con.close();
+    }
+}
+
