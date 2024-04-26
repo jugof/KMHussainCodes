d97153d3941ff329e2d2c262be5d1504f8103724
jugof <jugisoff@gmail.com>
2024-04-26 12:14:08 +0530

    26-04-2024RestApiTest

---
+++ CodingSamples/Java/Platform-Enterprise/RestApiTest/demo/src/main/java/app/rest/OrderResource.java
@@ -0,0 +1,15 @@
+package app.rest;
+
+public class OrderResource {
+    
+    public int orderNo;
+
+    public String orderDate;
+
+    public String customerId;
+
+    public int productNo;
+
+    public int quantity;
+}
+
---
+++ CodingSamples/Java/Platform-Enterprise/RestApiTest/demo/src/main/java/app/rest/SalesAgentModel.java
@@ -0,0 +1,51 @@
+package app.rest;
+
+import java.util.Collection;
+
+import io.grpc.ManagedChannelBuilder;
+import sales.OrderManagerGrpc;
+import sales.OrderManagerOuterClass.CustomerInput;
+import sales.OrderManagerOuterClass.OrderInput;
+
+public class SalesAgentModel {
+    
+    public void loadOrders(String customerId, Collection<OrderResource> entries) {
+        var channel = ManagedChannelBuilder.forAddress("localhost", 4030)
+                        .usePlaintext()
+                        .build();
+        try{
+            var remote = OrderManagerGrpc.newBlockingStub(channel);
+            var request = CustomerInput.newBuilder()
+                            .setCustomerCode(customerId)
+                            .build();
+            var reply = remote.fetchOrders(request);
+            reply.forEachRemaining(message -> {
+                var entry = new OrderResource();
+                entry.productNo = message.getItemCode();
+                entry.quantity = message.getItemCount();
+                entry.orderDate = message.getConfirmationDate();
+                entries.add(entry);
+            });
+        }finally{
+            channel.shutdown();
+        }
+    }
+
+    public void saveOrder(OrderResource entry) {
+        var channel = ManagedChannelBuilder.forAddress("localhost", 4030)
+                        .usePlaintext()
+                        .build();
+        try{
+            var remote = OrderManagerGrpc.newBlockingStub(channel);
+            var request = OrderInput.newBuilder()
+                            .setCustomerCode(entry.customerId)
+                            .setItemCode(entry.productNo)
+                            .setItemCount(entry.quantity)
+                            .build();
+            var reply = remote.placeOrder(request);
+            entry.orderNo =  reply.getConfirmationCode();
+        }finally{
+            channel.shutdown();
+        }
+    }
+}
