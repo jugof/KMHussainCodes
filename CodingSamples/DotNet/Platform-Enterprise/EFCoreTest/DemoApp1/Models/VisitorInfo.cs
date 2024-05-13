feb08a2144cf62538a98b54952df991f98c4412c
jugof <jugisoff@gmail.com>
2024-05-13 18:15:45 +0530

    EfCoreTest

---
+++ CodingSamples/DotNet/Platform-Enterprise/EFCoreTest/DemoApp1/Models/VisitorInfo.cs
@@ -0,0 +1,3 @@
+namespace DemoApp.Models;
+
+public readonly record struct VisitorInfo(string GivenName, string StarRating, int VisitCount, DateTime LastVisit);
---
+++ CodingSamples/DotNet/Platform-Enterprise/EFCoreTest/DemoApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoApp.Data;
+using DemoApp.Models;
+
+var model = new HotelModel();
+if(args.Length > 0)
+{
+    var guest = new Guest { Id = args[0] };
+    model.WriteVisitor(guest);
+    Console.WriteLine("Welcome {0}", guest.Id);
+}
+else
+{
+    foreach(var visitor in model.ReadVisitors())
+    {
+        Console.WriteLine("{0}\t{1}\t{2}", visitor.GivenName, visitor.VisitCount, visitor.LastVisit);
+    }
+}
